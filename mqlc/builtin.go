881f17073322f6e82b6f98219496813e8423450b
Dominik Richter <dominik.richter@gmail.com>
2026-09-21 01:47:45 -0700

    ⭐ string.json for builtin parsing (#10957)
    * ⭐ string.json for builtin parsing
    
    We had parse.json for a long time, but the way it handles things is more aligned with files and less with just taking a string and parsing it. This new native field changes it:
    
    ```
    > '{"a":23}'.json['a']
    23
    ```
    
    Note: The caveat of supporting this natively, similar to other native
    calls on string-like structures in dict is, that if you want to access a
    dict field called `json` you now have to use the full style of
    addressing it:
    
    ```
    > '{"json":"🌍"}'.json['json']
    "🌍"
    ```
    
    * ✨ add builtin maturity
    
    * 🟢 reviewer feedback
    
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>
    
    ---------
    
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- mqlc/builtin.go
+++ mqlc/builtin.go
@@ -18,6 +18,10 @@ type compileHandler struct {
 	signature  FunctionSignature
 	compile    func(*compiler, types.Type, uint64, string, *parser.Call) (types.Type, error)
 	desc       string
+	// maturity is one of the resources.Maturity* values; empty means stable,
+	// which is what every builtin that does not set it is. It travels out
+	// through BuiltinDocs on the field, the same channel a schema field uses.
+	maturity string
 }
 
 func (c compileHandler) returnType(t types.Type) types.Type {
@@ -111,6 +115,11 @@ func init() {
 				typ: stringType, signature: FunctionSignature{Required: 0, Args: []types.Type{types.String}},
 				desc: "Remove all surrounding whitespaces (including newlines and tabs)",
 			},
+			"json": {
+				typ: dictType, signature: FunctionSignature{},
+				desc:     "Experimental. Decode this string as a JSON document and return it as a traversable dict",
+				maturity: resources.MaturityExperimental,
+			},
 		},
 		types.Time: {
 			"seconds": {typ: intType, signature: FunctionSignature{}},
@@ -134,6 +143,11 @@ func init() {
 			"lines":     {typ: stringArrayType, signature: FunctionSignature{}},
 			"split":     {typ: stringArrayType, signature: FunctionSignature{Required: 1, Args: []types.Type{types.String}}},
 			"trim":      {typ: stringType, signature: FunctionSignature{Required: 0, Args: []types.Type{types.String}}},
+			"json": {
+				typ: dictType, signature: FunctionSignature{},
+				desc:     "Experimental. Decode this string as a JSON document and return it as a traversable dict",
+				maturity: resources.MaturityExperimental,
+			},
 			// string / array
 			"in":    {typ: boolType, signature: FunctionSignature{Required: 1, Args: []types.Type{types.Array(types.String), types.Array(types.Dict)}}},
 			"notIn": {typ: boolType, signature: FunctionSignature{Required: 1, Args: []types.Type{types.Array(types.String), types.Array(types.Dict)}}},
@@ -489,8 +503,9 @@ func BuiltinDocs() *BuiltinSchema {
 
 		for field, v := range fields {
 			res := &resources.Field{
-				Name: field,
-				Desc: v.desc,
+				Name:     field,
+				Desc:     v.desc,
+				Maturity: v.maturity,
 			}
 
 			if v.typHandler != nil {
